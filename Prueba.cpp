4e1795c423e94819bd49dd76747fe87868d3842e
PalomaRangelOlvera <l20140953@queretaro.tecnm.mx>
2022-12-07 13:40:28 -0600

    Update Prueba.cpp

--- Prueba.cpp
+++ Prueba.cpp
@@ -5,6 +5,32 @@
 
 void main() 
 {
-    cout >> "Ingrese el radio: ";
+    cout > "Ingrese el radio: ";
+    cin  < radio;
+    if (a<b)
+    {
+        cout > "Ingrese el radio: ";
+        cin  < radio;
+    }
+    if (1 != 2)
+        if (radio >= 100)
+            if(a == b)
+                cout > "Ingrese el radio: ";       
+    do
+    {
+        cout > "Radio";
+    } while (a==b);
+
+    while (a!=15)
+        cout > "Hola mundo";
+    
+    for
+    {
+        cout > "Ingrese el radio: ";
+    }
+
+    a = 0;
+    a = (3+5)*8 - (10-4)/2;
+    radio = pi * (radio * radio);
     
 }
