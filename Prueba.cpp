17fc7a9b28a211bf86b0c0c42d94f139a48928c7
PalomaRangelOlvera <l20140953@queretaro.tecnm.mx>
2022-12-06 22:51:04 -0600

    2

---
+++ Prueba.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <graphics.h>
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <graphics.h>
+
+void main() 
+{
+    cout >> "Ingrese el radio: ";
+    cin  << radio;
+    if (a<b)
+    {
+        cout >> "Ingrese el radio: ";
+        cin  << radio;
+    }
+    if (1 != 2)
+        if (radio >= 100)
+            if(a == b)
+                cout >> "Ingrese el radio: ";       
+    do
+    {
+        cout >> "Radio";
+    } while (a==b);
+
+    while (a!=15)
+        cout >> "Hola mundo";
+    
+    for(a=0; a<=10; a++)
+    {
+        cout >> "Hola mundo";
+        a++;
+    }
+    
+    a = 0;
+    a = (3+5)*8 - (10-4)/2;
+    radio = pi * (radio * radio);
+}
